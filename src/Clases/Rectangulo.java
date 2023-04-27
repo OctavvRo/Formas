208855440db975d09e3f3ca314690e4693a2a80c
Octavio Rosales <alavarooctavio@gmail.com>
2023-04-27 10:26:59 -0300

    Formas  primera subida

---
+++ src/Clases/Rectangulo.java
@@ -0,0 +1,24 @@
+
+package Clases;
+
+
+public class Rectangulo {
+    private double base;
+    private double altura;
+
+    public Rectangulo(double base, double altura) {
+        this.base = base;
+        this.altura = altura;
+    }
+    
+    
+    public double calcularArea(){
+        
+        return base*altura;
+        
+    }
+    public double calcularPerimetro(){
+        
+        return (2*base)+(2*altura);
+    }
+}
